b79bad42436a5e20d595952f9515a03c31a1b726
Solitaire <sol@sol-ab.localdomain>
2024-04-22 09:39:16 +0300

    Included sound processing dep

---
+++ AudioDeviceSelector.h
@@ -0,0 +1,30 @@
+#ifndef AUDIODEVICESELECTOR_H
+#define AUDIODEVICESELECTOR_H
+
+#include <QDialog>
+
+#include <q_io/audio_device.hpp>
+
+namespace q = cycfi::q;
+namespace Ui {
+class AudioDeviceSelector;
+}
+
+class AudioDeviceSelector : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AudioDeviceSelector(QWidget *parent = nullptr);
+    explicit AudioDeviceSelector(const std::vector<q::audio_device>& devices, QWidget *parent = nullptr);
+    ~AudioDeviceSelector();
+
+private slots:
+    void listSelected(const QModelIndex &index);
+
+private:
+    Ui::AudioDeviceSelector *ui;
+    std::vector<q::audio_device> m_audioDevices;
+};
+
+#endif // AUDIODEVICESELECTOR_H
--- MainWin.cpp
+++ MainWin.cpp
@@ -2,21 +2,21 @@
 
 #include "MainWin.h"
 #include "ui_MainWin.h"
-#include "LoginUi.h"
+#include "AudioDeviceSelector.h"
+
+
+#include <iostream>
+#include <typeinfo>
 
 MainWin::MainWin(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWin)
 {
     ui->setupUi(this);
-    LoginUi* test = new LoginUi();
-    QMdiSubWindow* sub = m_mdiArea->addSubWindow(test); // todo: mdiArea class for background support
-    sub->setWindowFlags(Qt::FramelessWindowHint);
-    QCoreApplication::processEvents();
-    int x = (this->geometry().width()) / 2  - m_mdiArea->geometry().width();
-    int y = (this->geometry().height()) / 2 - m_mdiArea->geometry().height();
-    sub->move(x, y);
-    test->show();
+    AudioDeviceSelector* selector = new AudioDeviceSelector(q::audio_device::list(), this);
+    ui->gl_main->addWidget(selector);
+    selector->setAttribute(Qt::WA_DeleteOnClose, true);
+    selector->show();
 }
 
 MainWin::~MainWin()
